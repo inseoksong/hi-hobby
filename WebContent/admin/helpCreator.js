e63d68aa88f2ec6ccd97b83512677c4aa0db7165
hanamanse <hana9589@gmail.com>
2022-05-23 18:47:13 +0900

    어드민 페이지 (완성)

---
+++ WebContent/admin/helpCreator.js
@@ -0,0 +1,20 @@
+const profile = document.querySelector('div.profile');
+const menu = document.querySelector('div.child-box');
+
+profile.addEventListener("click",function (){
+    if(menu.classList.contains('toggle')){
+        menu.classList.remove('toggle');
+    } else {
+        menu.classList.add('toggle');
+
+        const logOut = document.querySelector('div.logOut');
+
+        logOut.addEventListener("click", function (){
+            console.log("클릭됨");
+            window.open('101adminLogin.jsp','_self',true);
+        });
+    }
+});
+
+
+const numAr = document.querySelectorAll('button.numCircle');
