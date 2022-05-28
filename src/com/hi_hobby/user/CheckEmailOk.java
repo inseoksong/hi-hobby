9209d44b992565ed3040e939142362c62479e59a
조수민 <chosm0129@naver.com>
2022-05-28 16:40:11 +0900

    임시

--- src/com/hi_hobby/user/CheckEmailOk.java
+++ src/com/hi_hobby/user/CheckEmailOk.java
@@ -1,21 +1,16 @@
 package com.hi_hobby.user;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONObject;
-
 import com.hi_hobby.action.Action;
 import com.hi_hobby.action.ActionInfo;
-import com.hi_hobby.domain.dao.UserDAO;
 
 public class CheckEmailOk implements Action {
-	
-
-	 return null;
+	@Override
+	public ActionInfo execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		return null;
 	}
-
 }
--- src/com/hi_hobby/user/MypageModify.java
+++ src/com/hi_hobby/user/MypageModify.java
@@ -7,13 +7,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.hi_hobby.action.Action;
 import com.hi_hobby.action.ActionInfo;
-import com.hi_hobby.domain.dao.UserDAO;
-import com.hi_hobby.domain.vo.UserVO;
 
 public class MypageModify implements Action{
-	
-
-	 return null;
+	@Override
+	public ActionInfo execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		return null;
 	}
-
 }
